e0f7eda9c026a44024d278f94bc078540b01bcd7
nchristiny <icarus747@gmail.com>
2016-05-28 19:37:54 -0500

    Implement dotenv

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
 import axios from 'axios';
 
 // export const FETCH_USERS = 'FETCH_USERS';
-const ROOT_URL = 'http://localhost:3000';
+// const ROOT_URL = 'http://localhost:3000';
+axios.get('http://localhost:3000/users')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (response) {
+    console.log(response);
+  });
